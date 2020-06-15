5a22a1a89940ca8b748987d28c68550e3d67a443
Yasir Nadeem <m-yasir.dev@protonmail.com>
2020-06-15 13:14:53 +0500

    Cleanup code. Create Circle class for adding an animated circle. Create draw and update methods of class along with constructor and added relevant properties to the circle. Replaced hard coded code in animate function with new object oriented code.

--- canvas-anim/canvas.js
+++ canvas-anim/canvas.js
@@ -1,53 +1,107 @@
 const canvas = document.querySelector("canvas");
 
-const winHeight = window.innerHeight;
-const winWidth = window.innerWidth;
-
-canvas.height = winHeight;
-canvas.width = winWidth;
+canvas.height = innerHeight;
+canvas.width = innerWidth;
 
 // c = context
 const c = canvas.getContext("2d");
 
-/**
- *
- * @param {number} x
- * @param {number} y
- * @param {number} radius
- * @param {number} startAngle
- * @param {number} endAngle
- * @param {boolean} antiClockwise
- */
-function createArc(
-    x,
-    y,
-    radius,
-    startAngle,
-    endAngle,
-    antiClockwise = false,
-    color = `#${Math.floor(Math.random() * 25542195).toString(16)}`
-) {
-    c.beginPath();
-    c.arc(x, y, radius, startAngle, endAngle, antiClockwise);
-    c.strokeStyle = color;
-    c.stroke();
+class Circle {
+    antiClockwise;
+    color;
+    endAngle;
+    radius;
+    speed;
+    startAngle;
+    x;
+    xSign = 1;
+    y;
+    ySign = 1;
+
+    /**
+     *
+     * @param {number} x
+     * @param {number} y
+     * @param {number} radius
+     * @param {number} startAngle
+     * @param {number} endAngle
+     * @param {boolean} antiClockwise
+     * @param {string} color
+     * @param {number} speed
+     */
+
+    constructor(
+        x,
+        y,
+        radius,
+        startAngle,
+        endAngle,
+        speed = 5,
+        color = `#${Math.floor(Math.random() * 25542195).toString(16)}`,
+        antiClockwise = false
+    ) {
+        this.antiClockwise = antiClockwise;
+        this.color = color;
+        this.endAngle = endAngle;
+        this.radius = radius;
+        this.speed = speed;
+        this.startAngle = startAngle;
+        this.x = x;
+        this.y = y;
+    }
+
+    /**
+     *
+     * @param {number} x
+     * @param {number} y
+     * @param {number} radius
+     * @param {number} startAngle
+     * @param {number} endAngle
+     * @param {boolean} antiClockwise
+     */
+    createArc(
+        x,
+        y,
+        radius,
+        startAngle,
+        endAngle,
+        color = `#${Math.floor(Math.random() * 25542195).toString(16)}`,
+        antiClockwise = false
+    ) {
+        c.beginPath();
+        c.arc(x, y, radius, startAngle, endAngle, antiClockwise);
+        c.strokeStyle = color;
+        c.stroke();
+    }
+
+    draw() {
+        this.createArc(
+            this.x,
+            this.y,
+            this.radius,
+            this.startAngle,
+            this.endAngle,
+            this.color,
+            this.antiClockwise
+        );
+    }
+
+    update() {
+        if (this.x + 30 >= innerWidth || this.x - 30 <= 0)
+            this.xSign = -this.xSign;
+        if (this.y + 30 >= innerHeight || this.y - 30 <= 0)
+            this.ySign = -this.ySign;
+        this.x += this.xSign * this.speed;
+        this.y += this.ySign * this.speed;
+    }
 }
 
-let x = 200;
-let y = 200;
-let speed = 5;
-let xSign = 1;
-let ySign = 1;
+const circle = new Circle(200, 200, 30, 0, Math.PI * 2, 5, "pink");
 function animate() {
     requestAnimationFrame(animate);
-    c.clearRect(0, 0, winWidth, winHeight);
-    createArc(x, y, 30, 0, Math.PI * 2, false, "pink");
-    if (x + 30 >= winWidth || x - 30 <= 0) xSign = -xSign;
-    if (y + 30 >= winHeight || y - 30 <= 0) ySign = -ySign;
-    x += xSign * speed;
-    y += ySign * speed;
+    c.clearRect(0, 0, innerWidth, innerHeight);
+    circle.update();
+    circle.draw();
 }
-
-// createArc(200, 200, 30, 0, Math.PI * 2, false, "pink");
 
 animate();
